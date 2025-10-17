e806c710309fb69f4672213d2c10ec57741ac7c9
TersigniKev <tersignikevin.05@gmail.com>
2025-10-17 08:37:45 +0200

    tp-fini

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
 // Data retrieved from https://www.vikjavev.no/ver/#2020-04-15,2020-04-16
+const API_KEY ='8cf649b7f2f54da291281827251610'
+let date = new Date();
+let dateDuJour = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
+const historyWeatherData = async (location) => {
+    const result = await fetch(`https://api.weatherapi.com/v1/history.json?key=${API_KEY}&q=${location}&dt=${dateDuJour}`)
+    const data = await result.json()
+    return data;
+
+}
+const mtpdata = await historyWeatherData("Montpellier", dateDuJour); //on va chercher les donnees de montpellier dans l'API
+const mtpTemperature = []; //on creer un tableau mtptemperature
+mtpdata.forecast.forecastday[0].hour.forEach((hour) => { //on boucle sur toutes les heures du tableau et on ajoute les temperature pour
+    mtpTemperature.push(hour.temp_c);                    //chaques heures dans le tableau mtpTemperature.
+});
+
+const parisdata = await historyWeatherData("Paris", dateDuJour);
+const parisTemperature = [];
+parisdata.forecast.forecastday[0].hour.forEach((hour) => {
+    parisTemperature.push(hour.temp_c);
+});
+
+const lilledata = await historyWeatherData("Lille", dateDuJour);
+const lilleTemperature = [];
+lilledata.forecast.forecastday[0].hour.forEach((hour) => {
+    lilleTemperature.push(hour.temp_c);
+});
+
+
 
 Highcharts.chart('container', {
     chart: {
@@ -9,122 +38,85 @@ Highcharts.chart('container', {
         }
     },
     title: {
-        text: 'Temperature de la journee a Montpellier',
+        text: `Temperature de la journee Montpellier`,
         align: 'left'
     },
     subtitle: {
-        text: '29th of February, 2024 at two locations in Vik i Sogn, Norway',
+        text: new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0)),
         align: 'left'
     },
     xAxis: {
         type: 'datetime',
+        tickInterval: 3 * 3600 * 1000, 
         labels: {
             overflow: 'justify'
+        },
+        plotBands:[{ //nuit
+            from: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0),
+            to: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 6),
+            color: 'white',
+            label: {
+                text: 'nuit',
+                style:{
+                    opacity:0.7
+                }
+            }
+        },{ //matin
+           from: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 6),
+            to: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12),
+            color: 'rgba(7, 120, 233, 0.1)',
+            label: {
+                text: 'matin',
+                style:{
+                    opacity:0.7
+                }
+            }
+        },{ //midi
+           from: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12),
+            to: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 13),
+            color: 'white',
+            label: {
+                text: 'Midi',
+                style:{
+                    opacity:0.7
+                }
+            }
+        },{ //apres-midi
+           from: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 13),
+            to: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 19),
+            color: 'rgba(7, 120, 233, 0.1)',
+            label: {
+                text: 'Apres-midi',
+                style:{
+                    opacity:0.7
+                }
+            }
+        },{ //soir
+           from: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 19),
+            to: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 24),
+            color: 'white',
+            label: {
+                text: 'soir',
+                style:{
+                    opacity:0.7
+                }
+            }
         }
+    ]
     },
     yAxis: {
         title: {
-            text: 'Wind speed (m/s)'
+            text: 'Température (°C)'
         },
         minorGridLineWidth: 0,
         gridLineWidth: 0,
         alternateGridColor: null,
-        plotBands: [{ // Light air
-            from: 0.3,
-            to: 1.5,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Light air',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Light breeze
-            from: 1.5,
-            to: 3.3,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Light breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Gentle breeze
-            from: 3.3,
-            to: 5.5,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Gentle breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Moderate breeze
-            from: 5.5,
-            to: 8,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Moderate breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Fresh breeze
-            from: 8,
-            to: 11,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Fresh breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Strong breeze
-            from: 11,
-            to: 14,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Strong breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Near Gale
-            from: 14,
-            to: 17,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Near gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Fresh Gale
-            from: 17,
-            to: 20.5,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Fresh gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Strong Gale
-            from: 20.5,
-            to: 24,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Strong gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }]
+        plotBands: []
     },
     
     tooltip: {
         formatter: function(){
-          const icon = this.y > 10 ? '' : '';
+          const icon = this.y > 10 ? '☀️' : '❄️';
           return `<b>${this.series.name}</b><br/>
                   temperature : ${this.y} °c ${icon}`;
         },
@@ -143,27 +135,24 @@ Highcharts.chart('container', {
                 enabled: false
             },
             pointInterval: 3600000, // one hour
-            pointStart: '2014-02-29'
+            pointStart: Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0),
         }
     },
     series: [{
-        name: 'Hestavollane',
-        data: [
-            12.9, 13.8, 10.2, 8.4, 10.0, 9.2, 10.0,
-            12.2, 13.2, 12.7, 12.5, 11.4, 10.4,
-            7.9, 8.0, 11.4, 11.5, 12.0, 12.0,
-            10.4, 11.2, 11.5, 12.2, 11.5, 8.3
-        ]
+        name: "Montpellier",
+        data: mtpTemperature
+    },
+    { 
+        name: "Paris",
+        data: parisTemperature
+     },
+     { 
+        name: "Lille",
+        data: lilleTemperature
+     }
+    
+],
 
-    }, {
-        name: 'Vik',
-        data: [
-            null, 1.3, 1.1, 0.8, 1.8, 1.7, 0.8,
-            0.8, 1.0, 1.0, 1.0, 0.8, 1.4,
-            1.3, 2.9, 6.1, 6.4, 6.6, 6.4,
-            6.3, 5.4, 3.9, 3.0, 1.7, 1.4
-        ]
-    }],
     navigation: {
         menuItemStyle: {
             fontSize: '10px'
